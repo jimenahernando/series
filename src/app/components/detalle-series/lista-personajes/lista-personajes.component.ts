6baddbc9b36749d42694f28593fbe11f3660ade7
jimena <jimenahernando@hotmail.com>
2021-08-23 22:57:00 +0200

    Componente listaPersonajes

---
+++ src/app/components/detalle-series/lista-personajes/lista-personajes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Personaje } from 'src/app/interfaces/personaje.interface';
+import { PersonajesService } from 'src/app/services/personajes.service';
+
+@Component({
+  selector: 'app-lista-personajes',
+  templateUrl: './lista-personajes.component.html',
+  styleUrls: ['./lista-personajes.component.css']
+})
+export class ListaPersonajesComponent implements OnInit {
+
+  arrPersonajes: Personaje[];
+
+  constructor( 
+    private personajesService: PersonajesService,
+    private activatedRoute: ActivatedRoute ) {
+      this.arrPersonajes = []
+     }
+
+  ngOnInit(): void {
+    this.activatedRoute.parent?.params.subscribe(params => {
+      const idSerie = parseInt(params.serieId);
+      this.arrPersonajes = this.personajesService.getPersonajesBySerie(idSerie);
+    });
+  }
+
+
+}
