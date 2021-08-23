15b3deb536352dd55735948fbc371f2148545443
jimena <jimenahernando@hotmail.com>
2021-08-23 09:12:32 +0200

    Creo componentes: detalleSeries, filtros y listaSeries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FiltrosComponent } from './components/filtros/filtros.component';
+import { DetalleSeriesComponent } from './components/detalle-series/detalle-series.component';
+import { ListaSeriesComponent } from './components/lista-series/lista-series.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltrosComponent,
+    DetalleSeriesComponent,
+    ListaSeriesComponent
   ],
   imports: [
     BrowserModule,
