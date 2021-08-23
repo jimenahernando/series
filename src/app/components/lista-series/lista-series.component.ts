cb6507bc8d784b66fc9e942b4a9227d3a5e05b9e
jimena <jimenahernando@hotmail.com>
2021-08-23 22:25:56 +0200

    Componente listaSeries

--- src/app/components/lista-series/lista-series.component.ts
+++ src/app/components/lista-series/lista-series.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Serie } from 'src/app/interfaces/serie.interface';
+import { SeriesService } from 'src/app/services/series.service';
 
 @Component({
   selector: 'app-lista-series',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaSeriesComponent implements OnInit {
 
-  constructor() { }
+  arrSeries: Serie[];
+
+  constructor( private seriesServices: SeriesService) { 
+    this.arrSeries = [];
+  }
 
   ngOnInit(): void {
+    this.arrSeries = this.seriesServices.getAll();
+  }
+  
+  onFiltroRating(pRating: number){
+    this.arrSeries = this.seriesServices.getByRating(pRating);
+  }
+  
+  onFiltroCanal(pCanal: any){
+    if(pCanal){
+      this.arrSeries = this.seriesServices.getByCanal(pCanal);
+    } else {
+      this.arrSeries = this.seriesServices.getAll();
+    }
+  }
+
+  onBusqueda(pTitulo: any){
+    this.arrSeries = this.seriesServices.getByTitle(pTitulo);
   }
 
 }
