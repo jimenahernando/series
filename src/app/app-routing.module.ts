012989d9b656155a0f1d1a34d21a36b6488c474e
jimena <jimenahernando@hotmail.com>
2021-08-23 22:04:44 +0200

    Importo componentes y mapeo rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetalleSeriesComponent } from './components/detalle-series/detalle-series.component';
+import { ListaPersonajesComponent } from './components/detalle-series/lista-personajes/lista-personajes.component';
+import { ListaTemporadasComponent } from './components/detalle-series/lista-temporadas/lista-temporadas.component';
+import { ListaSeriesComponent } from './components/lista-series/lista-series.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/series' },
+  { path: 'series', component: ListaSeriesComponent },
+  { path: 'series/:serieId', component: DetalleSeriesComponent, children: 
+    [
+      { path: 'personajes', component: ListaPersonajesComponent }, 
+      { path: 'temporadas', component: ListaTemporadasComponent }
+    ] 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { FiltrosComponent } from './components/filtros/filtros.component';
 import { DetalleSeriesComponent } from './components/detalle-series/detalle-series.component';
 import { ListaSeriesComponent } from './components/lista-series/lista-series.component';
+import { ThumbnailComponent } from './components/thumbnail/thumbnail.component';
+import { FormsModule } from '@angular/forms';
+import { ListaPersonajesComponent } from './components/detalle-series/lista-personajes/lista-personajes.component';
+import { ListaTemporadasComponent } from './components/detalle-series/lista-temporadas/lista-temporadas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FiltrosComponent,
     DetalleSeriesComponent,
-    ListaSeriesComponent
+    ListaSeriesComponent,
+    ThumbnailComponent,
+    ListaPersonajesComponent,
+    ListaTemporadasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
